4306409237b31e49cc9f698b067dcdc37a737cdc
hatobus <s1240056@gmail.com>
2019-04-23 23:48:29 +0900

    tarekomiの実装です

--- entity/tarekomi.go
+++ entity/tarekomi.go
@@ -1,11 +1,12 @@
 package entity
 
 type Tarekomi struct {
-	ID          int64  `db:"id"`
-	Status      int64  `db:"status"`
-	Threshold   int64  `db:"threshold"`
-	URL         string `db:"url"`
-	Description string `db:"description"`
+	ID           int64  `db:"id"`
+	Status       int64  `db:"status"`
+	Threshold    int64  `db:"threshold"`
+	TargetUserID int64  `db:"targetuserid"`
+	URL          string `db:"url"`
+	Description  string `db:"description"`
 }
 
 func (t *Tarekomi) TableName() string {
---
+++ repository/sqlite_tarekomi.go
@@ -0,0 +1,151 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/NoahOrberg/evileye/entity"
+	pb "github.com/NoahOrberg/evileye/protobuf"
+	"github.com/jmoiron/sqlx"
+)
+
+var (
+	PENDING = 0
+)
+
+type sqliteTarekomiRepository struct {
+	db *sqlx.DB
+}
+
+func NewSqliteTarekomiRepository(db *sqlx.DB) TarekomiRepository {
+	return &sqliteTarekomiRepository{db}
+}
+
+func getUserByID(ctx context.Context, uid int64, db *sqlx.DB) (*entity.User, error) {
+	qstr := `SELECT * FROM users WHERE id = ?`
+	u := new(entity.User)
+
+	if err := db.Get(u, qstr, uid); err != nil {
+		return nil, err
+	}
+
+	return u, nil
+}
+
+func (r *sqliteTarekomiRepository) GetTarekomiFromUser(ctx context.Context, id, limit, offset int64) (pb.TarekomiSummaries, error) {
+
+	tk := make([]*pb.TarekomiSummary, 0, limit)
+	ts := new(pb.TarekomiSummaries)
+
+	u, err := getUserByID(ctx, id, r.db)
+	if err != nil {
+		return *ts, err
+	}
+
+	qstr := `SELECT * FROM tarekomi WHERE id = ? LIMIT ? OFFSET ?`
+
+	rows, err := r.db.Query(qstr, id, limit, offset)
+	if err != nil {
+		return *ts, err
+	}
+
+	for rows.Next() {
+		t := entity.Tarekomi{}
+
+		if err := rows.Scan(
+			&t.ID,
+			&t.Status,
+			&t.Threshold,
+			&t.TargetUserID,
+			&t.URL,
+			&t.Description,
+		); err != nil {
+			return *ts, err
+		}
+
+		tp := pb.TarekomiSummary{
+			Tarekomi: &pb.Tarekomi{
+				TargetUserId: t.TargetUserID,
+				Url:          t.URL,
+				Desc:         t.Description,
+			},
+			UserName: u.ScreenName,
+		}
+
+		tk = append(tk, &tp)
+	}
+
+	ts.Tarekomis = tk
+
+	return *ts, nil
+}
+
+func (r *sqliteTarekomiRepository) GetTarekomiBoard(ctx context.Context, limit, offset int64) (pb.TarekomiSummaries, error) {
+
+	qstr := `SELECT * FROM tarekomi WHERE status = 0 ORDER BY id LIMIT ? OFFSET ?`
+
+	tk := make([]*pb.TarekomiSummary, 0, limit)
+	ts := new(pb.TarekomiSummaries)
+
+	rows, err := r.db.Query(qstr, limit, offset)
+	if err != nil {
+		return *ts, err
+	}
+
+	for rows.Next() {
+		t := entity.Tarekomi{}
+
+		if err := rows.Scan(
+			&t.ID,
+			&t.Status,
+			&t.Threshold,
+			&t.TargetUserID,
+			&t.URL,
+			&t.Description,
+		); err != nil {
+			return *ts, err
+		}
+
+		n, err := getUserByID(ctx, t.TargetUserID, r.db)
+		if err != nil {
+			return *ts, err
+		}
+
+		tp := pb.TarekomiSummary{
+			Tarekomi: &pb.Tarekomi{
+				TargetUserId: t.TargetUserID,
+				Url:          t.URL,
+				Desc:         t.Description,
+			},
+			UserName: n.ScreenName,
+		}
+
+		tk = append(tk, &tp)
+	}
+
+	ts.Tarekomis = tk
+
+	return *ts, nil
+}
+
+// tarekomiを登録(投票街になるのでstatusはデフォで0)
+func (r *sqliteTarekomiRepository) Store(ctx context.Context, t entity.Tarekomi) (int64, error) {
+	qstr := `INSERT INTO tarekomi(status, threshold, targetuserid, url, description) VALUES(?, ?, ?, ?, ?)`
+
+	res, err := r.db.Exec(qstr, PENDING, t.Threshold, t.TargetUserID, t.URL, t.Description)
+	if err != nil {
+		return 0, err
+	}
+
+	return res.LastInsertId()
+}
+
+func (r *sqliteTarekomiRepository) UpdateTarekomi(ctx context.Context, newtarekomi entity.Tarekomi) (entity.Tarekomi, error) {
+	qstr := `UPDATE tarekomi SET state = ? WHERE id = ?`
+
+	_, err := r.db.Exec(qstr, newtarekomi.Status, newtarekomi.ID)
+	if err != nil {
+		return newtarekomi, err
+	}
+
+	return newtarekomi, nil
+}
