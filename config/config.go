a7d7b6821f1a38b4b1b7ba0abd53450b6c2d8e84
hatobus <s1240056@gmail.com>
2019-04-25 14:33:09 +0900

    setting enviromental value config file

--- config/config.go
+++ config/config.go
@@ -1,6 +1,32 @@
 package config
 
+import "github.com/kelseyhightower/envconfig"
+
 var (
 	CommitHash string
 	BuildAt    string
 )
+
+type Config struct {
+	Salt      string `default:"chu2byo"`
+	Secret    string `default:"welcome_to_underground"`
+	Threshold int64  `default:10`
+}
+
+var config Config
+
+func init() {
+	config, _ = Init()
+}
+
+func Init() (Config, error) {
+	err := envconfig.Process("Evileye", &config)
+	if err != nil {
+		return Config{}, err
+	}
+	return config, nil
+}
+
+func GetConfig() Config {
+	return config
+}
