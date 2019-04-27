ca772e286db2291d7c51a874030cf9c6de532921
NoahOrberg <noah.orberg.9999@gmail.com>
2019-04-27 17:40:26 +0900

    最強

--- repository/blocks.go
+++ repository/blocks.go
@@ -1,12 +1,18 @@
 package repository
 
 import (
+	"context"
+	"time"
+
 	"github.com/NoahOrberg/evileye/entity"
+	"github.com/NoahOrberg/evileye/log"
 	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
 )
 
 type Blocks interface {
 	GetLatestBlock() (*entity.Block, error)
+	InsertBlock(ctx context.Context, data, prevHash, hash string) (*entity.Block, error)
 }
 
 func NewBlocksRepository(db *sqlx.DB) Blocks {
@@ -32,5 +38,33 @@ func (b *blocks) GetLatestBlock() (*entity.Block, error) {
 		&block.Hash); err != nil {
 		return nil, err
 	}
+	return block, nil
+}
+
+func (b *blocks) InsertBlock(ctx context.Context,
+	data, prevHash, hash string) (*entity.Block, error) {
+	block := new(entity.Block)
+
+	block.Data = data
+	block.PrevHash = prevHash
+	block.Hash = hash
+	block.CreateTime = time.Now().Unix()
+
+	res, err := b.db.ExecContext(ctx,
+		`INSERT INTO blocks(data, prevhash, create_time, hash) VALUES (?, ?, ?, ?)`,
+		block.Data, block.PrevHash, block.CreateTime, block.Hash)
+	if err != nil {
+		log.L().Error("ExecContext in InsertBlock is failed",
+			zap.Error(err))
+		return nil, err
+	}
+
+	block.ID, err = res.LastInsertId()
+	if err != nil {
+		log.L().Error("InsertBlock response's LastInsertedID is failed",
+			zap.Error(err))
+		return nil, err
+	}
+
 	return block, nil
 }
